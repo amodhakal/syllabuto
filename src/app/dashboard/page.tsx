6dbe0da49647783cdf2b0ad09d0fb2e3357afb09
Amodh Dhakal <amodhakal@gmail.com>
2025-12-22 12:09:16 -0500

    Fixed route.ts name such that callback works

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,14 +2,5 @@ import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 
 export default async function page() {
-  const session = await auth.api.getSession({ headers: await headers() });
-  if (!session) {
-    await auth.api.signInSocial({
-      body: {
-        provider: "google",
-      },
-    });
-  }
-
   return <div className="">Dashboard</div>;
 }
