4d636ef618ecbc515d0bbe97ddd703f6752eaf15
Amodh Dhakal <amodhakal@gmail.com>
2025-12-22 11:37:34 -0500

    Setup google oauth

--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -3,6 +3,15 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/lib/db";
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      accessType: "offline",
+      prompt: "select_account consent",
+    },
+  },
   database: drizzleAdapter(db, {
     provider: "pg",
   }),
