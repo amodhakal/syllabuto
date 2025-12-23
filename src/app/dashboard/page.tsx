0a87457394bebff4370ada3bee0b84e4281c1dfa
Amodh Dhakal <amodhakal@gmail.com>
2025-12-22 22:39:30 -0500

    Get session info

--- src/app/dashboard/page.tsx
+++
@@ -1,6 +0,0 @@
-import { auth } from "@/lib/auth";
-import { headers } from "next/headers";
-
-export default async function page() {
-  return <div className="">Dashboard</div>;
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,33 @@
+"use client";
+
+import { authClient, useSession } from "@/lib/auth/client";
+
 export default function Home() {
+  const session = useSession();
+  if (session.isPending) {
+    return <div className="">Loading...</div>;
+  }
+
+  if (session.error) {
+    return <div className="">{session.error.message}</div>;
+  }
+
+  if (!session.data) {
+    return (
+      <div className="p-4 gap-4 ">
+        <p className="">You haven&apos;t signed in</p>
+        <button
+          onClick={async () =>
+            await authClient.signIn.social({ provider: "google" })
+          }
+          className=""
+        >
+          Sign in
+        </button>
+      </div>
+    );
+  }
+
+  const { token } = session.data.session;
   return <div className="font-black text-5xl">Syllabuto</div>;
 }
